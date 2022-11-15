2a38c376da7ca21726294cc8de90dcf89ebaccda
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-11-15 21:01:23 +1000

    Updated folder names

--- Client/src/Slide/ShoeCanvas.jsx
+++ Client/src/Slide/ShoeCanvas.jsx
@@ -13,10 +13,10 @@ function ShoeCanvas({modelName, cameraPos}) {
   )
 }
 
-const ShoeModel = async ({modelName, cameraPos}) => {
+const ShoeModel = ({modelName, cameraPos}) => {
   if (!modelName) modelName = "factors"; // fallback to default shoe
   let path = `./models/${modelName}.glb`;
-  const gltf = await useLoader(GLTFLoader, path)
+  const gltf = useLoader(GLTFLoader, path)
 
   // access THREE's camera...
   // note: can only useThree inside Canvas component
